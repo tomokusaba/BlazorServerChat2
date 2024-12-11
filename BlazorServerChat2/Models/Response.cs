a57436ff6395156620fdd57b32310b092348c598
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2024-12-11 21:43:18 +0900

    コメントアウトと初期化の追加
    `Response.cs` で複数クラスをコメントアウトしました。
    `Config.razor` で変数の初期化と nullable 参照型を導入しました。
    `Index.razor` で `SendGptChat` メソッドをコメントアウトしました。

--- BlazorServerChat2/Models/Response.cs
+++ BlazorServerChat2/Models/Response.cs
@@ -6,35 +6,35 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp15
 {
-    internal class Response
-    {
-        public string id { get; set; }
-        public string @object { get; set; }
+    //internal class Response
+    //{
+    //    public string id { get; set; }
+    //    public string @object { get; set; }
 
-        public int created { get; set; }
-        public string model { get; set; }
-        public List<Choise> choices { get; set; }
-        public Usage usage { get; set; }
+    //    public int created { get; set; }
+    //    public string model { get; set; }
+    //    public List<Choise> choices { get; set; }
+    //    public Usage usage { get; set; }
 
-    }
+    //}
 
-    internal class Choise
-    {
-        public int id { get; set; }
-        public string finish_reason { get; set; }
-        public Message message { get; set; }
-    }
+    //internal class Choise
+    //{
+    //    public int id { get; set; }
+    //    public string finish_reason { get; set; }
+    //    public Message message { get; set; }
+    //}
 
-    internal class Usage
-    {
-        public int prompt_tokens { get; set; }
-        public int completion_tokens { get; set; }
-        public int total_tokens { get; set; }
-    }
+    //internal class Usage
+    //{
+    //    public int prompt_tokens { get; set; }
+    //    public int completion_tokens { get; set; }
+    //    public int total_tokens { get; set; }
+    //}
 
-    internal class Message
-    {
-        public string role { get; set; }
-        public string content { get; set; }
-    }
+    //internal class Message
+    //{
+    //    public string role { get; set; }
+    //    public string content { get; set; }
+    //}
 }
